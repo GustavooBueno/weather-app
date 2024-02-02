6933e51a17bc331f4c19d10bb237aaaae91a8f7b
Gustavo Bueno <gustavo.ibis.gb@gmail.com>
2024-02-02 15:48:37 -0300

    Projeto Weather App com React

---
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,88 @@
+import "./WeatherApp.css"
+import search_icon from "../Assets/search.png"
+import snow_icon from "../Assets/snow.png"
+import wind_icon from "../Assets/wind.png"
+import rain_icon from "../Assets/rain.png"
+import humidity_icon from "../Assets/humidity.png"
+import drizzle_icon from "../Assets/drizzle.png"
+import cloud_icon from "../Assets/cloud.png"
+import clear_icon from "../Assets/clear.png"
+import { useState } from "react"
+import dotenv from "dotenv";
+dotenv.config();
+
+export default function WheatherApp () {
+    let api_key = process.env.REACT_APP_OPENWEATHER_API_KEY;
+
+    const [wicon, setWicon] = useState(cloud_icon)
+
+    const search = async () => {
+        const element = document.getElementsByClassName("cityInput")
+        if(element[0].value === "") return 0;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`
+
+        let response = await fetch (url);
+        let data = await response.json()
+        const humidity = document.getElementsByClassName("humidity-percent")
+        const wind = document.getElementsByClassName("wind-rate")
+        const temperature = document.getElementsByClassName("weather-temp")
+        const location = document.getElementsByClassName("weather-location")
+
+        humidity[0].innerHTML = data.main.humidity + " %"
+        wind[0].innerHTML = data.wind.speed + " km/h"
+        temperature[0].innerHTML = data.main.temp + "°c" 
+        location[0].innerHTML = data.name
+
+        const iconMappings = {
+            "01d": clear_icon,
+            "01n": clear_icon,
+            "02d": cloud_icon,
+            "02n": cloud_icon,
+            "03d": drizzle_icon,
+            "03n": drizzle_icon,
+            "04d": drizzle_icon,
+            "04n": drizzle_icon,
+            "09d": rain_icon,
+            "09n": rain_icon,
+            "10d": rain_icon,
+            "10n": rain_icon,
+            "13d": snow_icon,
+            "13n": snow_icon,
+          };
+
+        const weatherIcon = iconMappings[data.weather[0].icon] || clear_icon;
+        setWicon(weatherIcon);
+
+    }
+    return (
+        <div className="container">
+            <div className="top-bar">
+                <input type="text" className="cityInput" placeholder="Search"/>
+                <div className="search-icon" onClick={() => {search()}}>
+                    <img src={search_icon} alt="" />
+                </div>
+            </div>
+            <div className="weather-image">
+                <img src={wicon} alt="" />
+            </div>
+            <div className="weather-temp">24°</div>
+            <div className="weather-location">London</div>
+            <div className="data-container">
+                <div className="element">
+                    <img src={humidity_icon} alt="" className="icon"/>
+                    <div className="data">
+                        <div className="humidity-percent">64%</div>
+                        <div className="text">Humidity</div>
+                    </div>
+                </div>
+                <div className="element">
+                    <img src={wind_icon} alt="" className="icon"/>
+                    <div className="data">
+                        <div className="wind-rate">18 km/h</div>
+                        <div className="text">Wind Speed</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
